708fed11380634d74e185c12d522898a6ede5b75
Bruno Souza <brunoezully@gmail.com>
2023-10-12 20:01:24 -0300

    feat: seção de posts recentes

---
+++ components/Home/FeaturedPosts.tsx
@@ -0,0 +1,28 @@
+import { sortBlogs } from "@/utils";
+import BlogLayoutOne from "../Blogs/BlogLayoutOne";
+import BlogLayoutTwo from "../Blogs/BlogLayoutTwo";
+
+function FeaturedPosts({blogs}: any) {
+  const sortedBlogs = sortBlogs(blogs);
+
+  return (
+    <section className="w-full mt-32 px-32 flex flex-col item-center justify-center">
+      <h2 className="w-full inline-block font-bold capitalize text-4xl">
+        Posts em Destaque
+      </h2>
+      <div className="grid grid-cols-2 gap-6 mt-16">
+        <article className="col-span-1 row-span-2 relative">
+          <BlogLayoutOne blog={sortedBlogs[2]} />
+        </article>
+        <article className="col-span-1 row-span-1 relative">
+          <BlogLayoutTwo blog={sortedBlogs[3]} />
+        </article>
+        <article className="col-span-1 row-span-1 relative">
+        <BlogLayoutTwo blog={sortedBlogs[4]} />
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default FeaturedPosts
