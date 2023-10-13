d5a13118428e1c4fe17ae80490abf911cd7fd752
Bruno Souza <brunoezully@gmail.com>
2023-10-13 01:19:28 -0300

    feat: adicionado as novas seções a `home`

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import HomeCoverSection from "@/components/Home/HomeCoverSection";
 import { allBlogs } from "../.contentlayer/generated"
+import FeaturedPosts from "@/components/Home/FeaturedPosts";
+import RecentPosts from "@/components/Home/RecentPosts";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs}/>
+      <RecentPosts blogs={allBlogs}/>
     </main>
   )
 }
