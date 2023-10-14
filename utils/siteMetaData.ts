06d7cb4ad2043c0340d5e10bfa5f90b266028c78
Bruno Souza <brunoezully@gmail.com>
2023-10-14 18:11:48 -0300

    feat: metadata

---
+++ utils/siteMetaData.ts
@@ -0,0 +1,20 @@
+const siteMetaData = {
+  title: "Bruno Souza",
+  author: "Bruno Souza",
+  headerTitle: "Next.js Blog",
+  description: "Meu blog pessoal onde eu falo de programação e ciência.",
+  language: "pt-BR",
+  theme: "system", // system, dark or light
+  siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "brunoezully@gmail.com",
+  github: "https://github.com/darkmoonsk",
+  twitter: "https://twitter.com/brunobotelho10",
+  facebook: "https://www.facebook.com/bruno.souza.547389/",
+  linkedin: "https://www.linkedin.com/in/brunosouzadkm/",
+  portfolio: "https://portfolio.brunosouzadev.com/",
+  locale: "pt-BR",
+};
+
+export default siteMetaData;
