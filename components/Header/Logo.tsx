d417b2321dd12c3e3baa3f8d7cc982ba52bd9e4d
Bruno Souza <brunoezully@gmail.com>
2023-10-14 18:14:28 -0300

    fix: responsividade

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -4,11 +4,11 @@ import profileImg from "@/public/profile-img.png"
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center text-dark gap-3">
-        <div className="w-16 overflow-hidden rounded-full border border-solid border-dark">
+    <Link href="/" className="flex items-center text-dark dark:text-light gap-3">
+        <div className="w-12 md:w-16 overflow-hidden rounded-full border border-solid border-dark dark:border-light">
             <Image src={profileImg} alt="Bruno Souza" className="w-full h-auto rounded-full"/>
         </div>
-        <span className="font-bold text-xl">Bruno Souza</span>
+        <span className="font-bold dark:font-semibold text-lg md:text-xl">Bruno Souza</span>
     </Link>
   )
 }
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,36 +1,64 @@
+"use client"
 import Link from "next/link";
 import Logo from "./Logo";
 import { GithubIcon, LinkedinIcon, MoonIcon, SunIcon } from "../Icons";
 import portfolioImage from "@/public/portfolio.png";
 import Image from "next/image";
+import siteMetaData from "@/utils/siteMetaData";
+import { useThemeSwitch } from "../Hooks/useThemeSwitch";
+import { useContext, useState } from "react";
+import Menu from "./Menu";
 
 function Header() {
+  const [click, setClick] = useState(false);
   const iconAnimation = "hover:scale-125 transition-all ease-in-out duration-200";
 
+  const toggleClick = () => {
+    setClick(!click);
+  }
+
   return (
-    <header className="w-full p-4 px 10 flex items-center justify-between">
+    <header className="w-full p-4 px-5 sm:px-10 flex items-center justify-between">
       <Logo />
-      <nav
-        className="
-          flex items-center gap-2 w-max py-3 px-8 bg-light/80 backdrop-blur-sm
-          border border-solid border-dark rounded-full 
-          font-medium capitalize 
-          fixed top-6 right-1/2 translate-x-1/2 z-50
-      "
+      <button className="inline-block cursor-pointer lg:hidden z-50 bg-light"
+      onClick={toggleClick}
       >
-        <Link href="/" className="mr-2">Inicio</Link>
-        <Link href="/about"className="mx-2">Sobre</Link>
-        <Link href="/contact" className="mx-2">Contato</Link>
-        <button>
-          <SunIcon />
-        </button>
-      </nav>
-      
-      <div className="flex items-center gap-5">
+        <div className="w-6 transition-all ease duration-300">
+          <div className="relative">
+            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all
+            ease duration-200
+            "
+            style={{
+              transform: click ? "rotate(-45deg) translateY(0)" : "rotate(0) translateY(6px)"
+            }}
+            >&nbsp;</span>
+            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all
+            ease duration-200"
+            style={{
+              opacity: click ? 0 : 1
+            }}
+            >&nbsp;</span>
+            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all
+            ease duration-200"
+            style={{
+              transform: click ? "rotate(45deg) translateY(0)" : "rotate(0) translateY(-6px)"
+            }}
+            >&nbsp;</span>
+          </div>
+        </div>
+      </button>
+
+      {
+        click && <Menu isMobile />
+      }
+
+      <Menu />
+
+      <div className="items-center gap-5 hidden lg:flex">
         <a
-          href="https://www.linkedin.com/in/brunosouzadkm"
+          href={siteMetaData.linkedin}
           target="_blank"
-          className="flex items-center"
+          className="flex items-center text-dark dark:text-light"
         >
           <LinkedinIcon
             className={`
@@ -41,22 +69,22 @@ function Header() {
           Linkedin
         </a>
         <a
-          href="https://www.linkedin.com/in/brunosouzadkm"
+          href={siteMetaData.github}
           target="_blank"
-          className="flex items-center"
+          className="flex items-center text-dark dark:text-light"
         >
           <GithubIcon
             className={`
-            inline-block max-w-6 max-h-6 mr-2
+            inline-block max-w-6 max-h-6 mr-2 dark:fill-light
             ${iconAnimation}
             `}
           />
           Github
         </a>
         <a
-          href="https://www.linkedin.com/in/brunosouzadkm"
+          href={siteMetaData.portfolio}
           target="_blank"
-          className="flex items-center"
+          className="flex items-center text-dark dark:text-light"
         >
           <Image src = {portfolioImage} alt="Logo do portfolio"
             className={`
