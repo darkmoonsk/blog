489a0f6a7b068c8cdc329f1ea9c4a66b694fd463
Bruno Souza <brunoezully@gmail.com>
2023-10-14 18:08:36 -0300

    feat: add metadata infos

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import type { Metadata } from "next";
 import { Inter, Manrope } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { useEffect } from "react";
+import siteMetaData from "@/utils/siteMetaData";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -18,8 +19,40 @@ const manrope = Manrope({
 });
 
 export const metadata: Metadata = {
-  title: "Bruno Souza",
-  description: "Meu blog pessoal onde eu falo de programação e ciência.",
+  metadataBase: new URL(siteMetaData.siteUrl),
+  title: {
+    template: `%s | ${siteMetaData.title}`,
+    default: siteMetaData.title, 
+  },
+  description: siteMetaData.description,
+  openGraph: {
+    title: siteMetaData.title,
+    description: siteMetaData.description,
+    url: siteMetaData.siteUrl,
+    siteName: siteMetaData.title,
+    images: [
+      siteMetaData.socialBanner
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteMetaData.title,
+    images: [siteMetaData.socialBanner],
+  },
 };
 
 export default function RootLayout({
@@ -28,14 +61,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={cx(
           inter.variable,
           manrope.variable,
-          "font-mr bg-light "
+          "font-mr bg-light dark:bg-dark"
         )}
       >
+
+
+      <Script id="darkMode">
+        {
+          `
+            if (localStorage.getItem("theme") === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.documentElement.classList.add('dark')
+            } else {
+              document.documentElement.classList.remove('dark')
+            }
+          `
+        }
+      </Script>
+
         <Header />
         {children}
         <Footer />
