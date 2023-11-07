843dcd11ebbe7ac337c948f2b4075d1044b3e8ff
Bruno Souza <brunoezully@gmail.com>
2023-11-07 16:51:26 -0300

    fix: alteração do diretorio padrão

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
@@ -14,19 +14,19 @@ const config: Config = {
         light: "#FFF",
         accent: "#07bbd6",
         accentDark: "#FFDB4D",
-        gray: "#747474"
+        gray: "#747474",
       },
       fontFamily: {
         mr: ["var(--font-mr)"],
         in: ["var(--font-in)"],
       },
       animation: {
-        roll: "roll 24s linear infinite"
+        roll: "roll 24s linear infinite",
       },
       keyframes: {
         roll: {
-          "0%": {transform: "translateX(100%)"},
-          "100%": {transform: "translateX(-100%)"}
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       screens: {
@@ -35,13 +35,9 @@ const config: Config = {
       },
       backgroundImage: {
         galaxy: "url('/galaxy-bg.png')",
-      }
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
-}
-export default config
-
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
+export default config;
