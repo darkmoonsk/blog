17e281907ecf66b8d9451012403c46992024d8cb
Bruno Souza <brunoezully@gmail.com>
2023-11-08 17:06:13 -0300

    feat: Formulario de contato integrado

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,60 +1,91 @@
 "use client";
-import React from "react";
+import sendEmailParams from "@/app/models/sendEmailParams";
 import { useForm } from "react-hook-form";
+import SuccessModal from "./SuccessModal";
+import { useState } from "react";
 
 export default function ContactForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
-  const onSubmit = (data: {}) => console.log(data);
-  console.log(errors);
+    reset,
+  } = useForm<sendEmailParams>();
+
+  const [showModal, setShowModal] = useState(false);
+
+  const onSubmit = async (data: sendEmailParams) => {
+    const response = await fetch('/api/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+
+    reset();
+  
+    if (!response.ok) {
+      console.error(errors);
+    }
+
+    if(response.ok) {
+      console.log('Email enviado com sucesso')
+      setShowModal(true);
+    }
+  }
+  
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="mt-12 text-xl font-medium leading-relaxed font-in"
-    >
-      Olá! meu nome é{" "}
-      <input
-        type="text"
-        placeholder="Nome"
-        {...register("nome", { required: true, maxLength: 80 })}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b
+    <>
+      <SuccessModal isOpen={showModal} onClose={toggleModal} />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-12 text-xl font-medium leading-relaxed font-in"
+      >
+        Olá! meu nome é{" "}
+        <input
+          type="text"
+          placeholder="Nome"
+          {...register("name", { required: true, maxLength: 80 })}
+          className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b
+          border-gray focus:border-gray bg-transparent
+          "
+        />
+        e eu quero falar sobre um possivel projeto. você pode falar comigo pelo e-mail:
+        <input
+          type="text"
+          placeholder="Email"
+          {...register("email", { required: true })}
+          className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b
         border-gray focus:border-gray bg-transparent
         "
-      />
-      e eu quero falar sobre um possivel projeto. você pode falar comigo pelo e-mail:
-      <input
-        type="text"
-        placeholder="Email"
-        {...register("email", { required: true })}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b
-       border-gray focus:border-gray bg-transparent
-       "
-      />
-      ou pelo whatsapp através do numero
-      <input
-        type="number"
-        placeholder="Whatsapp"
-        {...register("whatsapp", { required: true })}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b
-        border-gray focus:border-gray bg-transparent
-        "
-      />
-      Aqui está os detalhes sobre o meu projeto <br/>
-      <textarea
-        {...register("description", {})}
-        rows={3}
-        placeholder="Meu projeto é sobre..."
-        className="w-full resize-none outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-lg border-b
-        border-gray focus:border-gray bg-transparent
-        "
-      />
-      <input type="submit" value="Enviar" className="mt-8 font-medium inline-block capitalize text-xl py-3 px-8 border-2 border-solid
-      border-dark dark:border-light rounded cursor-pointer
-      " />
-    </form>
+        />
+        ou pelo whatsapp através do numero
+        <input
+          type="number"
+          placeholder="Whatsapp"
+          {...register("whatsapp", { required: true })}
+          className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b
+          border-gray focus:border-gray bg-transparent
+          "
+        />
+        Aqui está os detalhes sobre o meu projeto <br/>
+        <textarea
+          {...register("description", {})}
+          rows={3}
+          placeholder="Meu projeto é sobre..."
+          className="w-full resize-none outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-lg border-b
+          border-gray focus:border-gray bg-transparent
+          "
+        />
+        <input type="submit" value="Enviar" className="mt-8 font-medium inline-block capitalize text-xl py-3 px-8 border-2 border-solid
+        border-dark dark:border-light rounded cursor-pointer
+        " />
+      </form>
+    </>
   );
 }
---
+++ src/components/Contact/SuccessModal.tsx
@@ -0,0 +1,48 @@
+interface SuccessModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const SuccessModal = ({ isOpen, onClose }: SuccessModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black/50 text-dark z-50 flex justify-center items-center">
+      <div className="bg-white px-16 py-8 rounded-md shadow-lg max-w-sm mx-auto">
+        <div className="flex flex-col items-center">
+          <svg
+            className="w-16 h-16 text-green-600 mb-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 13l4 4L19 7"
+            ></path>
+          </svg>
+          <h3 className="mb-5 text-2xl font-semibold text-gray-900">
+            Mensagem Enviada!
+          </h3>
+          <p className="mb-2 text-lg text-center text-gray-600">
+            Sua mensagem foi enviada com sucesso. Em breve estarei respondendo em seu E-mail.
+          </p>
+          <p className="mb-5 text-lg text-center text-gray-600">
+            
+          </p>
+          <button
+            onClick={onClose}
+            className=" text-light bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            Fechar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessModal;
